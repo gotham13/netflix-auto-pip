e41edc35f52f735452903e9aad504d298f3b900d
Abhinav Gautam <abhinav.nanhey@gmail.com>
2020-02-01 16:13:04 +0530

    bug fixes

--- src/popup.js
+++ src/popup.js
@@ -1,4 +1,4 @@
-function getBaseUrl(){
+function getBaseUrl() {
     return window.location.origin;
 }
 
@@ -8,19 +8,16 @@ $('#togglepip').click(()=>{
     });
 });
 
-    $('#nflxpip')
-        .checkbox({
+    $('#nflxpip').checkbox({
             onChecked:()=>{
                 localStorage.setItem("nflx","true");
             },
             onUnchecked:()=>{
                 localStorage.setItem("nflx","false");
             }
-        })
-    ;
+    });
 
-    $('#sitepip')
-        .checkbox({
+    $('#sitepip').checkbox({
             onChecked:()=>{
                 chrome.tabs.query({active:true, currentWindow:true},(tabs)=>{
                     chrome.tabs.executeScript(tabs[0].id,{
@@ -28,16 +25,18 @@ $('#togglepip').click(()=>{
                     }, function (results) {
                         if(chrome.runtime.lastError)
                             console.log("aaa");
-                        if(results[0].indexOf("netflix")!==-1)
-                            localStorage.setItem("nflx","true");
-                        else {
-                            let urls = localStorage.getItem("urls");
-                            if(urls !== null) {
-                                let url_arr = JSON.parse(urls);
-                                url_arr.push(results[0]);
-                                localStorage.setItem("urls",JSON.stringify(url_arr));
-                            } else {
-                                localStorage.setItem("urls",JSON.stringify([results[0]]));
+                        if(typeof results !== 'undefined') {
+                            if(results[0].indexOf("netflix")!==-1)
+                                localStorage.setItem("nflx","true");
+                            else {
+                                let urls = localStorage.getItem("urls");
+                                if(urls !== null) {
+                                    let url_arr = JSON.parse(urls);
+                                    url_arr.push(results[0]);
+                                    localStorage.setItem("urls",JSON.stringify(url_arr));
+                                } else {
+                                    localStorage.setItem("urls",JSON.stringify([results[0]]));
+                                }
                             }
                         }
                     });
@@ -50,21 +49,22 @@ $('#togglepip').click(()=>{
                     }, function (results) {
                         if(chrome.runtime.lastError)
                             console.log("aaa");
-                        if(results[0].indexOf("netflix")!==-1)
-                            localStorage.setItem("nflx","false");
-                        else {
-                            let urls = localStorage.getItem("urls");
-                            if(urls !== null) {
-                                let url_arr = JSON.parse(urls);
-                                url_arr = url_arr.filter(url=>url!==results[0]);
-                                localStorage.setItem("urls",JSON.stringify(url_arr));
+                        if(typeof results !== 'undefined') {
+                            if (results[0].indexOf("netflix") !== -1)
+                                localStorage.setItem("nflx", "false");
+                            else {
+                                let urls = localStorage.getItem("urls");
+                                if (urls !== null) {
+                                    let url_arr = JSON.parse(urls);
+                                    url_arr = url_arr.filter(url => url !== results[0]);
+                                    localStorage.setItem("urls", JSON.stringify(url_arr));
+                                }
                             }
                         }
                     });
                 });
             }
-        })
-    ;
+    });
 
     let nflx = localStorage.getItem("nflx");
     let urls = localStorage.getItem("urls");
@@ -74,18 +74,18 @@ $('#togglepip').click(()=>{
         }, function (results) {
             if(chrome.runtime.lastError)
                 console.log("aaa");
-                if(nflx !== null){
-                    if(nflx == "true")
+            if(typeof results !== 'undefined') {
+                if (nflx !== null) {
+                    if (nflx == "true")
                         $('#nflxpip')
-                            .checkbox("set checked");
-                }
+                            .checkbox("set checked");}
 
-                if(urls !== null) {
-                    let url_arr=JSON.parse(urls);
-                    url_arr = url_arr.filter(u=>u.indexOf(results[0])!==-1);
-                    if(url_arr.length>0)
+                if (urls !== null) {
+                    let url_arr = JSON.parse(urls);
+                    url_arr = url_arr.filter(u => u.indexOf(results[0]) !== -1);
+                    if (url_arr.length > 0)
                         $('#sitepip')
-                            .checkbox("set checked");
-                }
+                            .checkbox("set checked");}
+            }
         });
     });
